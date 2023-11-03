0716c211f71cdaf5e1ec89c8b578e913faa3167b
Max Heilmann <support@matix-media.net>
2023-11-03 19:20:46 +0100

    feat: :lipstick: remove menubar by default

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -13,6 +13,7 @@ function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
